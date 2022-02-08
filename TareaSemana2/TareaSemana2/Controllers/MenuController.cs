35ec90ffe3ec90df6993e87496123e0a876963be
eretana91 <eretana91@gmail.com>
2022-02-07 22:24:02 -0600

    Tarea Semana 2

---
+++ TareaSemana2/TareaSemana2/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TareaSemana2.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Games()
+        {
+            return View();
+        }
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
+    }
+}
