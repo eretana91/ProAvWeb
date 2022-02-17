7e71b25ca835fbfa75dbf7a81910c9ab0a562e86
eretana91 <eretana91@gmail.com>
2022-02-16 18:02:43 -0600

    Tarea Semana 3

---
+++ TareaSemana3/BootstrapHW/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BootstrapHW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Tareas/Tareas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
