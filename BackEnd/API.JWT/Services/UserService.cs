04e349af7c03572872f8c5462b51db4bb70149ce
eretana91 <eretana91@gmail.com>
2022-04-13 16:42:21 -0600

    Segundo Quiz
    Codigo de clase de reposicion y Quiz

---
+++ BackEnd/API.JWT/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using API.JWT.Models;
+
+namespace API.JWT.Services
+    {
+        public class UserService : IUserService
+        {
+            // Prueba de simulación, el valor predeterminado es verificación artificial efectiva
+            public bool IsValid(LoginRequestDTO req)
+            {
+                //Se debe agregar la verificacion hacia la base de datos 
+
+
+                return true;
+            }
+        }
+    }
